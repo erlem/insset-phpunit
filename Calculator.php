7584559da5c888ca27d4ba4244dae686952e5237
Jean-François ERLEM <>
2014-02-01 17:38:09 +0100

    Add Calculator.php

---
+++ Calculator.php
@@ -0,0 +1,23 @@
+<?php
+
+class Calculator {
+
+    public function add($val1, $val2) {
+        return $val1 + $val2;
+    }
+
+    public function subtract($val1, $val2) {
+        return $val1 - $val2;
+    }
+
+    public function multiply($val1, $val2) {
+        return $val1 * $val2;
+    }
+
+    public function divide($val1, $val2) {
+        return $val1 / $val2;
+    }
+
+}
+
+?>
